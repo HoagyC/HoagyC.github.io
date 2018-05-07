cac803c331cff72dcdb1c30695404cc5d4ce737e
hoagyc <hoagycunningham@gmail.com>
2018-05-07 23:41:09 +0200

    blah

--- form.php
+++ form.php
@@ -1,6 +1,10 @@
+<html>
+<body>
 <?php
 $myfile = fopen("content.csv", "a+") or die("Unable to open file!");
 $string = $_POST['name'].','.$_POST['location'].',['.$_POST['tags'].'],['.$_POST['reviews'].'].';
 fwrite($myfile);
 fclose($myfile);
 ?>
+</body>
+</html>
